4587da598c27e8d7fc33cf6817576eb2bbe231a0
Ryan Chase <ryanfcha@usc.edu>
2014-07-03 18:15:42 -0700

    added user back to login class

--- src/gui/LogIn.java
+++ src/gui/LogIn.java
@@ -24,9 +24,11 @@ public class LogIn extends JFrame {
 	private JTextField usernameTF;
 	private JTextField passwordTF;
 	public JButton logInButton;
+	private User user;
 	//public static LogIn log;
-	public LogIn(){
+	public LogIn(User user){
 		super("Login");
+		this.user = user;
 	}
 //    public void pullThePlug() {
 //        WindowEvent wev = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
@@ -69,7 +71,7 @@ public class LogIn extends JFrame {
 				user.sendLogInRequest();
 			}
 		}
-		logInButton.addActionListener(new LogInListener());
+		logInButton.addActionListener(new LogInListener(user));
 		
 		
 
